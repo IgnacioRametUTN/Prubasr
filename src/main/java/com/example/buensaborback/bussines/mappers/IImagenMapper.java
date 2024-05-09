243255f7744782b9c3c1e6a4ffab327a9d10deb9
FrancoAlbornoz <francod.albornoz@alumnos.frm.utn.edu.ar>
2024-05-09 19:32:28 -0300

    Se borran Comentarios

--- src/main/java/com/example/buensaborback/bussines/mappers/IImagenMapper.java
+++ src/main/java/com/example/buensaborback/bussines/mappers/IImagenMapper.java
@@ -8,13 +8,5 @@ import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface IImagenMapper {
-//    @Mapping(target = "url", source = "source.url")
-//    String ImagenToImagenFullDto(Imagen source);
-//
-//    @Mapping( source = "source" ,target = "imagen.url")
-//    Imagen ImagenFullDtoToImagen(String source);
-//
-//    List<String> ImagensToImagenFullDtos(List<Imagen> source);
-//
-//    List<Imagen> ImagenFullDtosToImagens(List<String> ImagenFullDtos);
+
 }
