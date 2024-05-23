f61449d5e4e3d111d3074175c330daa678501de1
FrancoAlbornoz <francod.albornoz@alumnos.frm.utn.edu.ar>
2024-05-23 06:57:25 -0300

    Se Agregan Implementaciones de Sucursal

---
+++ src/main/java/com/example/buensaborback/bussines/service/impl/SucursalServiceImpl.java
@@ -0,0 +1,7 @@
+package com.example.buensaborback.bussines.service.impl;
+import com.example.buensaborback.bussines.service.ISucursalService;
+import com.example.buensaborback.domain.entities.Sucursal;
+
+public class SucursalServiceImpl extends BaseServiceImpl<Sucursal,Long> implements ISucursalService {
+
+}
