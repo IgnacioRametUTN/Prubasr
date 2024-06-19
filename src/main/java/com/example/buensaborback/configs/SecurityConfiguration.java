6c1e1365821e0c5b352e87bd73671790a1db78fe
IgnacioRametUTN <ignacio.ramet@alumnos.frm.utn.edu.ar>
2024-06-19 13:24:53 -0300

    Update security

--- src/main/java/com/example/buensaborback/configs/SecurityConfiguration.java
+++ src/main/java/com/example/buensaborback/configs/SecurityConfiguration.java
@@ -43,9 +43,108 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
 // TODO agregar rutas
+                                // Definir reglas para ArticuloInsumoController
+                                .requestMatchers(HttpMethod.GET, "/api/articulos/insumos").hasAuthority("SCOPE_read:articulos")
+                                .requestMatchers(HttpMethod.GET, "/api/articulos/insumos/**").hasAuthority("SCOPE_read:articulos")
+                                .requestMatchers(HttpMethod.POST, "/api/articulos/insumos").hasAuthority("SCOPE_write:articulos")
+                                .requestMatchers(HttpMethod.POST, "/api/articulos/insumos/bulk").hasAuthority("SCOPE_write:articulos")
+                                .requestMatchers(HttpMethod.POST, "/api/articulos/insumos/uploads").hasAuthority("SCOPE_write:articulos")
+                                .requestMatchers(HttpMethod.PUT, "/api/articulos/insumos/**").hasAuthority("SCOPE_write:articulos")
+                                .requestMatchers(HttpMethod.DELETE, "/api/articulos/insumos/**").hasAuthority("SCOPE_delete:articulos")
+                                .requestMatchers(HttpMethod.GET, "/api/articulos/insumos/search").hasAuthority("SCOPE_read:articulos")
 
+                                // Definir reglas para ArticuloManufacturadoController
 
-                                .requestMatchers(HttpMethod.GET, "/promociones").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/articulos/manufacturados").hasAuthority("SCOPE_read:articulos")
+                                .requestMatchers(HttpMethod.GET, "/api/articulos/manufacturados/**").hasAuthority("SCOPE_read:articulos")
+                                .requestMatchers(HttpMethod.POST, "/api/articulos/manufacturados").hasAuthority("SCOPE_write:articulos")
+                                .requestMatchers(HttpMethod.POST, "/api/articulos/manufacturados/bulk").hasAuthority("SCOPE_write:articulos")
+                                .requestMatchers(HttpMethod.PUT, "/api/articulos/manufacturados/**").hasAuthority("SCOPE_write:articulos")
+                                .requestMatchers(HttpMethod.DELETE, "/api/articulos/manufacturados/**").hasAuthority("SCOPE_delete:articulos")
+                                .requestMatchers(HttpMethod.GET, "/api/articulos/manufacturados/search").hasAuthority("SCOPE_read:articulos")
+
+                                // Categorias
+                                .requestMatchers(HttpMethod.GET, "/api/categorias").hasAuthority("SCOPE_read:categorias")
+                                .requestMatchers(HttpMethod.GET, "/api/categorias/padres").hasAuthority("SCOPE_read:categorias")
+                                .requestMatchers(HttpMethod.GET, "/api/categorias/alta").hasAuthority("SCOPE_read:categorias")
+                                .requestMatchers(HttpMethod.GET, "/api/categorias/**").hasAuthority("SCOPE_read:categorias")
+                                .requestMatchers(HttpMethod.PUT, "/api/categorias/**").hasAuthority("SCOPE_write:categorias")
+                                .requestMatchers(HttpMethod.POST, "/api/categorias/**").hasAuthority("SCOPE_write:categorias")
+                                .requestMatchers(HttpMethod.POST, "/api/categorias/bulk").hasAuthority("SCOPE_write:categorias")
+                                .requestMatchers(HttpMethod.DELETE, "/api/categorias/**").hasAuthority("SCOPE_delete:categorias")
+
+                                //Clientes
+                                .requestMatchers(HttpMethod.GET, "/api/clientes").hasAuthority("SCOPE_read:clientes")
+                                .requestMatchers(HttpMethod.GET, "/api/clientes/**").hasAuthority("SCOPE_read:clientes")
+                                .requestMatchers(HttpMethod.POST, "/api/clientes").hasAuthority("SCOPE_write:clientes")
+                                .requestMatchers(HttpMethod.PUT, "/api/clientes/**").hasAuthority("SCOPE_write:clientes")
+                                .requestMatchers(HttpMethod.DELETE, "/api/clientes/**").hasAuthority("SCOPE_delete:clientes")
+
+                                //Domicilios
+                                .requestMatchers(HttpMethod.GET, "/api/domicilios").hasAuthority("SCOPE_read:domicilios")
+                                .requestMatchers(HttpMethod.GET, "/api/domicilios/alta").hasAuthority("SCOPE_read:domicilios")
+                                .requestMatchers(HttpMethod.GET, "/api/domicilios/**").hasAuthority("SCOPE_read:domicilios")
+                                .requestMatchers(HttpMethod.PUT, "/api/domicilios/**").hasAuthority("SCOPE_write:domicilios")
+                                .requestMatchers(HttpMethod.POST, "/api/domicilios").hasAuthority("SCOPE_write:domicilios")
+                                .requestMatchers(HttpMethod.DELETE, "/api/domicilios/**").hasAuthority("SCOPE_delete:domicilios")
+
+                                //Empresa
+                                .requestMatchers(HttpMethod.GET, "/api/empresas").hasAuthority("SCOPE_read:empresas")
+                                .requestMatchers(HttpMethod.GET, "/api/empresas/**").hasAuthority("SCOPE_read:empresas")
+                                .requestMatchers(HttpMethod.POST, "/api/empresas").hasAuthority("SCOPE_write:empresas")
+                                .requestMatchers(HttpMethod.PUT, "/api/empresas/**").hasAuthority("SCOPE_write:empresas")
+                                .requestMatchers(HttpMethod.DELETE, "/api/empresas/**").hasAuthority("SCOPE_delete:empresas")
+
+                                //Mercado Pago
+                                .requestMatchers(HttpMethod.POST, "/api/mercado-pago/crear_preference_mp").hasAuthority("SCOPE_write:mercado-pago")
+
+                                //Pedido
+                                .requestMatchers(HttpMethod.POST, "/api/pedidos").hasAuthority("SCOPE_write:pedidos")
+                                .requestMatchers(HttpMethod.GET, "/api/pedidos").hasAuthority("SCOPE_read:pedidos")
+                                .requestMatchers(HttpMethod.GET, "/api/pedidos/**").hasAuthority("SCOPE_read:pedidos")
+                                .requestMatchers(HttpMethod.PUT, "/api/pedidos/actualizar/**").hasAuthority("SCOPE_write:pedidos")
+
+                                //Promocion
+                                .requestMatchers(HttpMethod.GET, "/api/promociones").hasAuthority("SCOPE_read:promociones")
+                                .requestMatchers(HttpMethod.GET, "/api/promociones/**").hasAuthority("SCOPE_read:promociones")
+                                .requestMatchers(HttpMethod.POST, "/api/promociones").hasAuthority("SCOPE_write:promociones")
+                                .requestMatchers(HttpMethod.PUT, "/api/promociones/**").hasAuthority("SCOPE_write:promociones")
+                                .requestMatchers(HttpMethod.DELETE, "/api/promociones/**").hasAuthority("SCOPE_delete:promociones")
+
+                                //PromocionDetalle
+                                .requestMatchers(HttpMethod.GET, "/api/promociones-detalles").hasAuthority("SCOPE_read:promociones-detalles")
+                                .requestMatchers(HttpMethod.GET, "/api/promociones-detalles/**").hasAuthority("SCOPE_read:promociones-detalles")
+                                .requestMatchers(HttpMethod.POST, "/api/promociones-detalles").hasAuthority("SCOPE_write:promociones-detalles")
+                                .requestMatchers(HttpMethod.PUT, "/api/promociones-detalles/**").hasAuthority("SCOPE_write:promociones-detalles")
+                                .requestMatchers(HttpMethod.DELETE, "/api/promociones-detalles/**").hasAuthority("SCOPE_delete:promociones-detalles")
+
+                                //Reportes
+                                .requestMatchers(HttpMethod.GET, "/api/reportes/top-products").hasAuthority("SCOPE_read:reportes")
+                                .requestMatchers(HttpMethod.GET, "/api/reportes/ReporteTotales").hasAuthority("SCOPE_read:reportes")
+
+                                //Sucursal
+                                .requestMatchers(HttpMethod.POST, "/api/sucursales").hasAuthority("SCOPE_write:sucursales")
+                                .requestMatchers(HttpMethod.GET, "/api/sucursales/{id}").hasAuthority("SCOPE_read:sucursales")
+                                .requestMatchers(HttpMethod.GET, "/api/sucursales").hasAuthority("SCOPE_read:sucursales")
+                                .requestMatchers(HttpMethod.PUT, "/api/sucursales/{id}").hasAuthority("SCOPE_write:sucursales")
+                                .requestMatchers(HttpMethod.DELETE, "/api/sucursales/{id}").hasAuthority("SCOPE_write:sucursales")
+                                .requestMatchers(HttpMethod.GET, "/api/sucursales/empresa/{empresaId}").hasAuthority("SCOPE_read:sucursales")
+
+                                //Unidad Medida
+                                .requestMatchers(HttpMethod.GET, "/api/unidades-medida").hasAuthority("SCOPE_read:unidades-medida")
+                                .requestMatchers(HttpMethod.GET, "/api/unidades-medida/alta").hasAuthority("SCOPE_read:unidades-medida")
+                                .requestMatchers(HttpMethod.GET, "/api/unidades-medida/{id}").hasAuthority("SCOPE_read:unidades-medida")
+                                .requestMatchers(HttpMethod.PUT, "/api/unidades-medida/{id}").hasAuthority("SCOPE_write:unidades-medida")
+                                .requestMatchers(HttpMethod.POST, "/api/unidades-medida").hasAuthority("SCOPE_write:unidades-medida")
+                                .requestMatchers(HttpMethod.POST, "/api/unidades-medida/bulk").hasAuthority("SCOPE_write:unidades-medida")
+                                .requestMatchers(HttpMethod.DELETE, "/api/unidades-medida/{id}").hasAuthority("SCOPE_write:unidades-medida")
+
+                                //Usuario
+                                .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/api/auth/register").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/auth/validar/{nombreUsuario}").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/auth/cliente/{nombreUsuario}").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/auth").hasAuthority("SCOPE_read:usuarios")
 
 
                                 .anyRequest().permitAll()
