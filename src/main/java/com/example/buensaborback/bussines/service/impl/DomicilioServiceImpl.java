07d0d8bfa3dfbd9aa22ec6b199739d3c2ec81faa
FrancoAlbornoz <francod.albornoz@alumnos.frm.utn.edu.ar>
2024-08-13 15:41:55 -0300

    Fix sucursal Domicilio

--- src/main/java/com/example/buensaborback/bussines/service/impl/DomicilioServiceImpl.java
+++ src/main/java/com/example/buensaborback/bussines/service/impl/DomicilioServiceImpl.java
@@ -1,16 +1,14 @@
 package com.example.buensaborback.bussines.service.impl;
 
 import com.example.buensaborback.bussines.service.IDomicilioService;
+import com.example.buensaborback.bussines.service.ILocalidadService;
 import com.example.buensaborback.domain.entities.Domicilio;
-import com.example.buensaborback.domain.entities.Localidad;
-import com.example.buensaborback.domain.entities.Pais;
-import com.example.buensaborback.domain.entities.Provincia;
 import com.example.buensaborback.presentation.advice.exception.NotFoundException;
 import com.example.buensaborback.repositories.DomicilioRepository;
 import com.example.buensaborback.repositories.LocalidadRepository;
 import com.example.buensaborback.repositories.PaisRepository;
 import com.example.buensaborback.repositories.ProvinciaRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,10 +17,13 @@ import java.util.List;
 public class DomicilioServiceImpl implements IDomicilioService {
     private final DomicilioRepository domicilioRepository;
 
+    private final ILocalidadService localidadService;
 
-    public DomicilioServiceImpl(DomicilioRepository domicilioRepository, LocalidadRepository localidadRepository, ProvinciaRepository provinciaRepository, PaisRepository paisRepository) {
+
+    public DomicilioServiceImpl(DomicilioRepository domicilioRepository, LocalidadRepository localidadRepository, ProvinciaRepository provinciaRepository, PaisRepository paisRepository, ILocalidadService localidadService) {
         this.domicilioRepository = domicilioRepository;
 
+        this.localidadService = localidadService;
     }
 
 
@@ -51,11 +52,27 @@ public class DomicilioServiceImpl implements IDomicilioService {
 
     @Override
     public Domicilio update(Long id, Domicilio body) {
+        // Verificar si el domicilio existe por el id
         this.existsDomicilioById(id);
-        return this.domicilioRepository.save(body);
+
+        // Obtener el domicilio existente de la base de datos
+        Domicilio existingDomicilio = this.domicilioRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Domicilio no encontrado con id: " + id));
+
+        body.setLocalidad(localidadService.getLocalidadById(body.getLocalidad().getId()));
+        // Actualizar los campos del domicilio existente con los del body
+        existingDomicilio.setLocalidad(body.getLocalidad());
+        existingDomicilio.setAlta(body.isAlta());
+        existingDomicilio.setCp(body.getCp());
+        existingDomicilio.setNumero(body.getNumero());
+        existingDomicilio.setClientes(body.getClientes());
+        existingDomicilio.setCalle(body.getCalle());
+        // Guardar el domicilio actualizado en la base de datos
+        return this.domicilioRepository.save(existingDomicilio);
     }
     @Override
     public Domicilio create(Domicilio body) {
+        body.setLocalidad(localidadService.getLocalidadById(body.getLocalidad().getId()));
         return this.domicilioRepository.save(body);
     }
     @Override
