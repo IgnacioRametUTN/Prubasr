1a171df2b207009a4c39b8d3fce75b88cb794fec
IgnacioRametUTN <ignacio.ramet@alumnos.frm.utn.edu.ar>
2024-10-31 23:02:17 -0300

    config test

--- src/main/java/com/example/buensaborback/configs/CloudinaryConfig.java
+++ src/main/java/com/example/buensaborback/configs/CloudinaryConfig.java
@@ -3,19 +3,24 @@ package com.example.buensaborback.configs;
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CloudinaryConfig {
 
+    @Value("${cloudinary.cloud_name}")
+    private String cloudName;
+
+    @Value("${cloudinary.api_key}")
+    private String apiKey;
+
+    @Value("${cloudinary.api_secret}")
+    private String apiSecret;
+
     @Bean
     public Cloudinary cloudinary() {
-        Dotenv dotenv = Dotenv.load();
-        String cloudName = dotenv.get("CLOUDINARY_CLOUD_NAME");
-        String apiKey = dotenv.get("CLOUDINARY_API_KEY");
-        String apiSecret = dotenv.get("CLOUDINARY_SECRET");
-
         return new Cloudinary(ObjectUtils.asMap(
                 "cloud_name", cloudName,
                 "api_key", apiKey,
